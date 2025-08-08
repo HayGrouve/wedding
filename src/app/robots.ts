c528f635913dac052f93a3177f26cfa61cce9f7b
haygrouve <haygrouve@gmail.com>
2025-08-08 10:39:49 +0300

    fix: Update base URLs in robots and sitemap files to reflect new deployment site

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://wedding-ana-maria-ivan.vercel.app";
+  const baseUrl = "https://wedding-delta-drab.vercel.app";
 
   return {
     rules: {
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://wedding-ana-maria-ivan.vercel.app";
+  const baseUrl = "https://wedding-delta-drab.vercel.app";
   const lastModified = new Date();
 
   return [
